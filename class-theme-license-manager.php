0823868f6cb5eb302e072952010318793c6bc518
Jip Moors <jhp.moors@gmail.com>
2017-02-16 21:33:51 +0100

    Provides custom license page capability control

--- class-theme-license-manager.php
+++ class-theme-license-manager.php
@@ -11,7 +11,7 @@ if ( class_exists( 'Yoast_License_Manager' ) && ! class_exists( "Yoast_Theme_Lic
 			if ( $this->license_is_valid() ) {
 				// setup auto updater
 				require_once dirname( __FILE__ ) . '/class-update-manager.php';
-				require_once dirname( __FILE__ ) . '/class-theme-update-manager.php'; // @TODO: Autoload?
+				require_once dirname( __FILE__ ) . '/class-theme-update-manager.php';
 				new Yoast_Theme_Update_Manager( $this->product, $this );
 			}
 		}
@@ -31,7 +31,12 @@ if ( class_exists( 'Yoast_License_Manager' ) && ! class_exists( "Yoast_Theme_Lic
 		 * Add license page and add it to Themes menu
 		 */
 		public function add_license_menu() {
-			add_theme_page( sprintf( __( '%s License', $this->product->get_text_domain() ), $this->product->get_item_name() ), __( 'Theme License', $this->product->get_text_domain() ), 'manage_options', 'theme-license', array( $this, 'show_license_page' ) );
+			$capability = 'manage_options';
+			if ( method_exists( $this->product, 'get_license_page_capability' ) ) {
+				$capability = $this->product->get_license_page_capability();
+			}
+
+			add_theme_page( sprintf( __( '%s License', $this->product->get_text_domain() ), $this->product->get_item_name() ), __( 'Theme License', $this->product->get_text_domain() ), $capability, 'theme-license', array( $this, 'show_license_page' ) );
 		}
 
 		/**
